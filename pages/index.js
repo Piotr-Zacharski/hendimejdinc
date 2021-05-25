030fcdd1ffaac4d25bddd771bad232d99527fe1b
Piotr-Zacharski <zacharski.p.h@gmail.com>
2021-05-25 15:02:19 +0200

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>HendiMejdi</title>
+      </Head>
+      <h1>HendiMejdi</h1>
+    </div>
+  )
+}
