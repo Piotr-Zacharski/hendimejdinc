dd68831127384ab3d6c1954e15c76e2f2df5e113
Piotr-Zacharski <zacharski.p.h@gmail.com>
2021-08-08 13:58:13 +0200

    Added star rating

--- components/StarRating.js
+++ components/StarRating.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { FaStar } from "react-icons/fa";
 
 const Star = ({ selected = false, onSelect = f => f}) => (
-    <FaStar color={selected ? "yellow" : "grey"} onClick={onSelect} />
+    <FaStar color={selected ? "gold" : "grey"} onClick={onSelect} />
 );
 
 const createArray = length => [...Array(length)];
