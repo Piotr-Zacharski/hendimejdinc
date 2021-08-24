dcc25ac880347072d11902411b3f2d894221bc13
Piotr-Zacharski <zacharski.p.h@gmail.com>
2021-08-24 11:26:46 +0200

    IDK

--- components/Colors.js
+++ components/Colors.js
@@ -1,12 +1,12 @@
 import Image from 'next/image'
-import pimo1 from '../public/pimo1.jpg';
+import pimo1 from '../public/pimo1.jpg'
 
 const Colors = () => {
     return (
         <div>
-            <Image src={pimo1} alt="pimo" height={30} width={30}/>
+            <Image src={pimo1} alt="pimo" height={30} width={30} />
         </div>
-     );
+    )
 }
 
-export default Colors;
+export default Colors
