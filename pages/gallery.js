f4bc569bc76c34d059cdfd5c6589438ebaba7227
Piotr-Zacharski <zacharski.p.h@gmail.com>
2021-09-02 21:39:01 +0200

    Without logo

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import ImageSlider from '../components/ImageSlider/ImageSlider'
-import { SliderData } from '../components/ImageSlider/SliderData'
+import Image from '../components/Image'
+
 
 
 
 export default function Gallery() {
     return (
       <div>
-            <ImageSlider slides={SliderData} />
+            <Image />
         </div>
     )
 }
