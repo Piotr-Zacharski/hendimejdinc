95a06ee588b4a26b3d17d58538f73e4ec3f45798
Piotr-Zacharski <62928418+Piotr-Zacharski@users.noreply.github.com>
2023-01-05 16:31:34 +0100

    :lipstick: new color palette

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import Image from '../components/Image'
+import Colors from "../components/Colors";
 
 export default function Gallery() {
     return (
       <div>
-            <Image />
+          <Colors />
         </div>
     )
 }
